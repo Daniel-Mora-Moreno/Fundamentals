52aa31482e22e330b8b90ecb976de206fa68bc7e
Daniel Mora <daniel27mora96@gmail.com>
2022-11-02 10:38:46 +0100

    Revert "Small fixes"
    This reverts commit de0c9348f991e8948c3d66a2fc8d8c019e64ba16.

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -2,32 +2,13 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
-    TextView textSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        textSignup = findViewById(R.id.textSignup);
-
-        textSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SignUp();
-            }
-        });
-    }
-
-    public void SignUp(){
-        Intent intent = new Intent(this, SignupActivity.class);
-        startActivity(intent);
     }
 }
