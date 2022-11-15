4809b81885ac445bdb8ff46d50111f2d040f94b8
Daniel Mora <daniel27mora96@gmail.com>
2022-11-15 19:19:10 +0100

    gradient added to signup, added Glide for icon and background in LoginActivity

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,6 +27,22 @@ public class LoginActivity extends AppCompatActivity {
                 openSignup();
             }
         });
+
+        ImageView mDog = findViewById(R.id.dog);
+        ImageView mLogo = findViewById(R.id.logo);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1561037404-61cd46aa615b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+//                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+                .into(mDog);
+
+        Glide.with(this)
+                .load(R.drawable.avocado_del_diablo)
+//                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mLogo);
 
     }
 
